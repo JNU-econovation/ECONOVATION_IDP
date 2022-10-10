7afe3a59ecca3ccb24830a15ccbe17dbed4b1f5a
= <ymecca730135@gmail.com>
2022-10-11 00:03:14 +0900

    hotfix : docker name 최신환경으로 변경

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 ./gradlew build -x test
 sudo docker login
-sudo docker build --build-arg DEPENDENCY=build/dependency -t blackbean99/t_econo .
-sudo docker push blackbean99/econovation_idp:zenbook
-sudo docker run --cap-add=NET_ADMIN -it -p 8080:8080 blackbean99/econovation_idp:latest
+sudo docker build --build-arg DEPENDENCY=build/dependency -t blackbean99/econovation_idp .
+sudo docker push blackbean99/econovation_idp
+sudo docker run --cap-add=NET_ADMIN -it -p 8080:8080 blackbean99/econovation_idp:latestg
