3e4e743780b9ff932f1863164a1f4e93fb861011
unknown <ymecca12@gmail.com>
2022-11-04 14:41:13 +0900

    feat : deploy Script redis issue fix

---
+++ redisOn.sh
@@ -0,0 +1,2 @@
+#1/bin/bash
+sudo nohup docker run --rm -d -p 6379:6379 redis &
--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
+#!bin/bash
 ./gradlew build -x test
 sudo docker login
+sudo bash ./redisOn.sh
 sudo docker build --build-arg DEPENDENCY=build/dependency -t blackbean99/econovation_idp .
 sudo docker push blackbean99/econovation_idp
-sudo docker run --cap-add=NET_ADMIN -it -p 8080:8080 blackbean99/econovation_idp:latest
+sudo nohup docker run --rm --cap-add=NET_ADMIN -i -p 8080:8080 blackbean99/econovation_idp:latest > ./idp.log 2>&1 &
